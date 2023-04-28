932c84c2cf2c05691ada27931f12d7d21c3ec954
Isaac Hemans <mostarcher@gmail.com>
2023-04-28 19:38:23 +0200

    final submission

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInLine(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 5
 
 
 class LessonInline(admin.StackedInline):
@@ -13,18 +23,27 @@ class LessonInline(admin.StackedInline):
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
+    list_display = ("name", "pub_date")
+    list_filter = ["pub_date"]
+    search_fields = ["name", "description"]
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ["title"]
 
 
 # <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["content", "is_correct"]
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
